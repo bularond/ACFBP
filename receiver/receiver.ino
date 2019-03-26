fabf1a1c663e3fdb0e8af1d7e972b159cae4e0e3
bularond <bularond@gmail.com>
2019-03-26 22:56:28 +0500

    Working version of project before incapsulating

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,28 +1,176 @@
 #include <SPI.h>
 #include <RF24.h>
+#include "DFPlayer_Mini_Mp3.h"
+#include <SoftwareSerial.h>
 
-RF24 radio(9, A10);
+ #define DEBUG_INCOME
+// #define DEBUG_SPEECH
 
+#define BUTTON 2
+#define BUTTON_GND 3
+
+#define MAX_WORD_IN_SPEECH 4
+
+RF24 radio(10, 9);
 const uint32_t pipe = 123456789;
 
-byte data[5];
+SoftwareSerial MP3(7, 8); // RX, TX
 
-void setup()
-{
-  radio.begin();
-  radio.setDataRate(RF24_1MBPS);
-  radio.setCRCLength(RF24_CRC_16);
-  radio.setPALevel(RF24_PA_MAX);
+int to_speech[MAX_WORD_IN_SPEECH] = {200, 60, 6, -1};
 
-  radio.setChannel(100);
-  radio.setAutoAck(false);
-  radio.powerUp();
-  radio.openWritingPipe(pipe);
+char my_number[5] = {'1', '6', 0, 0, 0};
+
+char data[5];
+
+
+int string_to_int(String inp, int st = 0){
+    int out = 0;
+    for(int i = st; i < inp.length(); i++){
+        out *= 10;
+        out += inp[i] - '0';
+    }
+    return out;
 }
 
-void loop()   
-{
-  for(byte i = 0; i < 5 ; i++) data[i] = i;
-  radio.write(&data, sizeof(data));
-  delay(500);
+void set_speech_from_number(){
+    set_speech(string_to_int(my_number));
+}
+
+void set_speech(int in){
+    #ifdef DEBUG_SPEECH
+    Serial.print("Set speech ");
+    Serial.println(in);
+    #endif
+    int pos_to_speech = 0;
+    if(in / 100){ // 3 sight
+        to_speech[pos_to_speech] = in - in % 100;
+        in -= to_speech[pos_to_speech];
+        pos_to_speech++;
+    }
+    if(in <= 19 && in){
+        to_speech[pos_to_speech] = in;
+        pos_to_speech++;
+    }
+    else if(in){
+        to_speech[pos_to_speech] = in - in % 10;
+        in -= to_speech[pos_to_speech];
+        pos_to_speech++;
+        to_speech[pos_to_speech] = in;
+        pos_to_speech++;
+    }
+    while(pos_to_speech < MAX_WORD_IN_SPEECH){
+        to_speech[pos_to_speech] = -1;
+        pos_to_speech++;
+    }
+    #ifdef DEBUG_SPEECH
+    Serial.print("Now speech ");
+    for(int i = 0; i < MAX_WORD_IN_SPEECH; i++){
+        Serial.print(to_speech[i]);
+        Serial.print(' ');
+    }
+    Serial.println();
+    #endif
+}
+
+void get_speech(){
+    mp3_play(0);
+    delay(1670);
+    mp3_stop();
+    delay(100);
+    for (int i = 0; to_speech[i] != -1; i++){
+        #ifdef DEBUG_SPEECH        
+        Serial.print("Talk ");
+        Serial.println(to_speech[i]);
+        #endif
+        mp3_play(to_speech[i]);
+        delay(1050);
+        mp3_stop();
+        delay(100);
+    }
+}
+
+void comand_handler(String inp){
+    switch (inp[0])
+    {
+        case 'v':
+            mp3_set_volume(string_to_int(inp, 1));
+            break;
+        case 's':
+            set_speech(string_to_int(inp, 1));
+            for(int i = 0; i < 5; i++)
+                my_number[i] = 0;
+            for(int i = 1; i < inp.length(); i++)
+                my_number[i - 1] = inp[i];
+            set_speech_from_number();
+            break;
+        case 'g':
+            Serial.println("Geting speech");
+            get_speech();
+            break;
+    }
+}
+
+bool check(){
+    for (int i = 0; i < 5; i++)
+        if (my_number[i] != data[i])
+            return false;
+    return true;
+}
+
+void getData(){
+    if (radio.available())
+    {
+        Serial.println("INCOME");
+        radio.read(&data, sizeof(data));
+        #ifdef DEBUG_INCOME
+        Serial.print(data[0]);
+        Serial.print(data[1]);
+        Serial.print(data[2]);
+        Serial.print(data[3]);
+        Serial.print(data[4]);
+        Serial.print('\n');
+        #endif
+    }
+    else
+        data[0] = 0;
+}
+
+void setup(){
+    Serial.begin(9600);
+
+    pinMode(11, INPUT_PULLUP);
+    
+    MP3.begin(9600);
+    mp3_set_serial(MP3); // активируем сериал порт с модулем
+    mp3_set_volume(30);  // ставим начальную громкость 30
+    delay(100);
+
+    radio.begin();
+    radio.setDataRate(RF24_1MBPS);
+    radio.setCRCLength(RF24_CRC_8);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setChannel(5);
+    radio.setAutoAck(false);
+    radio.powerUp();
+    radio.openReadingPipe(1, pipe);
+    radio.startListening();
+
+    pinMode(4, INPUT_PULLUP);
+    set_speech_from_number();
+}
+
+unsigned long last_met = 0;
+
+void loop(){
+    if(Serial.available())
+        comand_handler(Serial.readString());
+    getData();
+    if (check())
+        last_met = millis();
+    if (digitalRead(4) == 0 && millis() - last_met <= 20000)
+    {
+        Serial.print("INCOMING MESAGE \n");
+        get_speech();
+        while (digitalRead(4) == 0);
+    }
 }
--- transmitor/transmitor.ino
+++ transmitor/transmitor.ino
@@ -3,80 +3,138 @@
 #include <RF24.h>
 #include <Keypad.h>
 
-RF24 radio(A2,A3);
+RF24 radio(10, 9);
 const uint32_t pipe = 123456789;
 
 #define TYPE_INPUT 1
 
-#define VIBRO_PIN A0
-#define GROUND A1
-
+#define VIBRO_PIN A1
+#define GROUND A0
+#define STANDART 100
+#define DEL 50
+#define CLEAR 100
+#define SEND 200
+#define ERR 150
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
+/*char keys[ROWS][COLS] = {
+    {'1', '4', '7', 'd'},
+    {'2', '5', '8', '0'},
+    {'3', '6', '9', 'c'},
+    {'s', 's', 's', 's'}};*/
 char keys[ROWS][COLS] = {
-  {'1', '2', '3', '#'},
-  {'4', '5', '6', '#'},
-  {'7', '8', '9', '#'},
-  {'#', '#', '#', '#'}
-};
-byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {6, 7, 8, 9}; //connect to the column pinouts of the keypad
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+    {'1', '2', '3', 's'},
+    {'4', '5', '6', 's'},
+    {'7', '8', '9', 's'},
+    {'d', '0', 'c', 's'}};
+byte rowPins[ROWS] = {3, 4, 5, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {A2, A3, 8, 7}; //connect to the column pinouts of the keypad
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-
-char real_number[5]  = {0};
+char real_number[5] = {0};
 char write_number[5] = {0};
-int now_pos = 0;
+int pos = 0;
 
-
-void update_number(){
-  #ifdef TYPE_INPUT == 1
-    char key = keypad.getKey();
-
-    if (key != NO_KEY) {
-      if(key == '#'){
-        now_pos = 0;
-        for(int i = 0; i < 5; i++){
-          real_number[i] = write_number[i];
-          write_number[i] = 0;
-        }
-      }
-      else{
-        write_number[now_pos] = key;
-        now_pos++;
-        now_pos %= 5;
-      }
+void vibro(int ml, int repit = 0)
+{
+    digitalWrite(VIBRO_PIN, HIGH);
+    delay(ml);
+    digitalWrite(GROUND, HIGH);
+    delay(10);
+    digitalWrite(VIBRO_PIN, LOW);
+    digitalWrite(GROUND, LOW);
+    for (int i = 0; i < repit; i++)
+    {
+        delay(70);
+        digitalWrite(VIBRO_PIN, HIGH);
+        delay(ml + STANDART);
+        digitalWrite(GROUND, HIGH);
+        delay(10);
+        digitalWrite(VIBRO_PIN, LOW);
+        digitalWrite(GROUND, LOW);
     }
-  #endif
 }
 
-
-void send_number(){
-  radio.write(&real_number, sizeof(real_number));
+void update_number()
+{
+    char key = keypad.getKey();
+    if (key != NO_KEY)
+    {
+        vibro(STANDART);
+        switch (key)
+        {
+        case 's':
+            if (pos)
+            {
+                pos = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    real_number[i] = write_number[i];
+                    write_number[i] = 0;
+                }
+                vibro(SEND, 1);
+            }
+            else
+                vibro(ERR, 2);
+            break;
+        case 'd':
+            write_number[max(0, pos - 1)] = 0;
+            pos = max(pos - 1, 0);
+            vibro(DEL);
+            break;
+        case 'c':
+            pos = 0;
+            for (int i = 0; i < 5; i++)
+                write_number[i] = 0;
+            vibro(CLEAR);
+            break;
+        default:
+            if (pos == 0 && key == '0')
+                ;
+            else if (pos != 5)
+            {
+                write_number[pos] = key;
+                pos++;
+            }
+            else
+                vibro(ERR, 2);
+        }
+    }
 }
 
-
-void setup() {
-  pinMode(VIBRO_PIN, OUTPUT);
-  pinMode(GROUND, OUTPUT);
-  digitalWrite(GROUND, 0);
-
-  Serial.begin(9600);
-  radio.begin();
-  delay(2000);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setCRCLength(RF24_CRC_8);
-  radio.setPALevel(RF24_PA_HIGH);
-
-  radio.setChannel(100);
-  radio.setAutoAck(false);
-  radio.powerUp();
-  radio.openWritingPipe(pipe);
+void send_number()
+{
+    radio.write(&real_number, sizeof(real_number));
 }
 
-void loop() {
-  update_number();
-  Serial.println(real_number);
-  send_number();
+void setup()
+{
+    pinMode(VIBRO_PIN, OUTPUT);
+    pinMode(GROUND, OUTPUT);
+    digitalWrite(GROUND, 0);
+
+    Serial.begin(9600);
+    radio.begin();
+    delay(2000);
+    radio.setDataRate(RF24_1MBPS);
+    radio.setCRCLength(RF24_CRC_8);
+    radio.setPALevel(RF24_PA_MAX);
+
+    radio.setChannel(5);
+    radio.setAutoAck(false);
+    radio.powerUp();
+    radio.openWritingPipe(pipe);
+}
+
+void loop()
+{
+    update_number();
+    for (int i = 0; i < 5; i++)
+    {
+        Serial.print(write_number[i]);
+    }
+    Serial.println();
+    send_number();
+    delay(1);
 }
